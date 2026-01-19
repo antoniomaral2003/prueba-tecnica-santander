c7041d5bcf65a5043301275cf3ef52202f6a4a57
antoniomaral2003 <antoniomaral2003@gmail.com>
2026-01-19 09:54:40 +0100

    environment production

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://candidates-api-xdw5.onrender.com/candidates'
-};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/candidates'
+  production: true,
+  apiUrl: (window as any)['env']?.['apiUrl'] || 'https://candidates-api-xdw5.onrender.com/candidates'
 };
